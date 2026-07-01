2dd1dd92620cc5ac28a549740b69a0c09ae96fd2
Arsucar <630042479@qq.com>
2026-07-01 23:55:31 +0800

    release: v2.3.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "me.arsucar.rikka"
         minSdk = 26
         targetSdk = 37
-        versionCode = 174
-        versionName = "2.3.12"
+        versionCode = 175
+        versionName = "2.3.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
