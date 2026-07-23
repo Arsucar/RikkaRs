896c1f83fbf65980bac618111a56169e4786a979
Arsucar <630042479@qq.com>
2026-07-23 15:50:40 +0800

    chore(release): bump version to v2.3.37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "me.arsucar.rikka"
         minSdk = 26
         targetSdk = 37
-        versionCode = 198
-        versionName = "2.3.36"
+        versionCode = 199
+        versionName = "2.3.37"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
